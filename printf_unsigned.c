13c562ddac76ccd73f6985dbe245552757123e27
nancywambui <nwambuimuthee@gmail.com>
2023-08-22 12:14:00 -0700

    printf_unsigned.c

--- printf_unsigned.c
+++ printf_unsigned.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * printf_unsigned - prints integer
- * @args: argument to print
- * Return: number of characters printed
+ * printf_unsigned - prints integer of a digit
+ * @args: argument
+ * Return: number of characters
  */
 int printf_unsigned(va_list args)
 {
