a4cc160298e87c2bff3f3cf98881dd5e8c8dc4be
nancywambui <nwambuimuthee@gmail.com>
2023-08-22 11:58:18 -0700

    printf_oct.c

--- printf_oct.c
+++ printf_oct.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * printf_oct - prints an octal number.
- * @val: arguments.
- * Return: counter.
+ * printf_oct - prints an octal digit
+ * @val: argument
+ * Return: counter number
  */
 int printf_oct(va_list val)
 {
