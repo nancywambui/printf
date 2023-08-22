b7637d07528d8433154add42b6f4f85cec822e52
nancywambui <nwambuimuthee@gmail.com>
2023-08-22 12:03:01 -0700

    printf_rot13.c

--- printf_rot13.c
+++ printf_rot13.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * printf_rot13 - printf str to ROT13 place into buffer
- * @args: type struct va_arg where is allocated printf arguments
- * Return: counter
+ * printf_rot13 - print str to ROT13 
+ * @args: type struct va_argument
+ * Return: counter digit
  *
  */
 int printf_rot13(va_list args)
